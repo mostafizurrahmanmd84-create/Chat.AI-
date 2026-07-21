9cba24c95f4862d57ecf69fa609a0cf8aabaebff
mostafizurrahmanmd84-create <mostafizurrahmanmd84@gmail.com>
2026-07-22 01:27:13 +0600

    Change base path in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Mostafizur-chat-AI/',
+  base: '/Chat.AI-/',
   plugins: [react()],
 })
